29f9adc688aba927a8a8cdc9aa15ef032249b328
edgarw19 <edgarallenpoed@gmail.com>
2014-06-15 01:19:46 -0700

    stuffs

---
+++ TutorRank/models/user.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema;
+var passportLocalMongoose = require('passport-local-mongoose');
+
+
+var User = new Schema({
+	email: String,
+	password: String
+	//{type: String, set :function(newValue) {
+		//return Hash.isHashed(newValue) ? newValue : Hash.generate(newValue);
+	}
+);
+
+User.plugin(passportLocalMongoose);
+
+/*UserSchema.statics.authenticate = function(email, password, callback) {
+	this.findOne({ email: email }, function(error, user) {
+		if (user && Hash.verify(password, user.password)) {
+			callback(null, user);
+		} else if (user || !error) {
+			// Email or password was invalid (no MongoDB error)
+			error = new Error("Your email address or password is invalid. Please try again.");
+			callback(error, null);
+		} else {
+			// Something bad happened with MongoDB. You shouldn't run into this often.
+			callback(error, null);
+		}
+	});
+};*/
+
+module.exports = mongoose.model('User', User);
